905c2ed3754ef20bc95063c936ad8a2d0f981f52
Thilagamithun <thilakamithun@gmail.com>
2023-10-29 11:29:08 +0530

    first commmit

---
+++ Guvi3/src/pack1/Prog1.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class Prog1 {
+	
+	public void display() {
+		System.out.println("HELLO");
+	}
+	
+public static void main(String[] args) {
+	Prog1 p = new Prog1();
+	p.display();
+
+}
+	
+	
+}
